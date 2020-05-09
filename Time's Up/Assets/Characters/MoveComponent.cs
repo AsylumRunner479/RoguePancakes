e4f540e214a779921cb6c1ed64df01b15892137d
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-05-08 20:56:54 -0400

    Rudementary Movement and gitignore fixed

---
+++ Time's Up/Assets/Characters/MoveComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveComponent : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    public float Speed;
+
+    [Header("Testing Variables")]
+    public Vector3 Direction;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(transform.position + (Direction * Speed * Time.deltaTime));
+        Direction = Vector3.zero;
+    }
+
+    public void SetDirection(Vector3 _direction) { Direction = _direction; }
+}
---
+++ Time's Up/Assets/Characters/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        GetComponent<MoveComponent>().SetDirection(direction);
+    }
+}
