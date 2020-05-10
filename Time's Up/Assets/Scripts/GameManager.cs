b694d780fab32373db3aee5c898fc23fd681f639
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-05-10 10:03:13 +1000

    created main scene

---
+++ Time's Up/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool isDead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Dying()
+    {
+        isDead = true;
+    }
+    public void Revive()
+    {
+        isDead = false;
+    }
+}
---
+++ Time's Up/Assets/Scripts/Time Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            GameManager.isDead = true;
+        }
+    }
+}
