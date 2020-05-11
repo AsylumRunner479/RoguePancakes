22d596ef1977f6687291443c432ce2c415fa8a57
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-05-11 14:39:22 +1000

    attempted fixing

---
+++ Time's Up/Assets/Scripts/ElevatorManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorManager : MonoBehaviour
+{
+    public static bool PlayerSafe;
+    public Collider2D col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerSafe = false;   
+    }
+    public void OnColliderEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerSafe = true;
+        } 
+    }
+    public void OnColliderExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerSafe = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
