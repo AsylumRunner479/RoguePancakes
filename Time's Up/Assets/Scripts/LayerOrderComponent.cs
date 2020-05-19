9d073f4a891f0b5b057ba74ee396972b1340308c
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2020-05-19 00:24:47 -0400

    Crafting foundation

--- Time's Up/Assets/Scripts/LayerOrderComponent.cs
+++ Time's Up/Assets/Scripts/LayerOrderComponent.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LayerOrderComponent : MonoBehaviour
 {
+    [Header("Visible Variables")]
+    public SpriteRenderer SpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpriteRenderer.sortingOrder = (int) -transform.position.y;
     }
 }
