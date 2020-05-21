ab590f64c5c323da65da2b9a547b999338481df1
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-05-21 14:44:31 +1000

    added minor texture alterations

---
+++ Time's Up/Assets/Scripts/Shooting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float damage, range, attackSpeed, attackRate;
+    public Camera cam;
+    public LayerMask enemy;
+    public ParticleSystem bullets;
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackSpeed += Time.deltaTime;
+        if (Input.GetMouseButton(0) && attackSpeed > 0)
+        {
+            attackSpeed = -3f / attackRate;
+            Shoot();
+            bullets.Emit(1);
+
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        EnemyAI enemy = other.transform.GetComponent<EnemyAI>();
+        Material skin = other.transform.GetComponent<Material>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            skin.SetFloat("Damaged", enemy.curHealth);
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            EnemyAI enemy = hit.transform.GetComponent<EnemyAI>();
+            Material skin = hit.transform.GetComponent<Material>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+                skin.SetFloat("Damaged", enemy.curHealth);
+            }
+        }
+    }
+}
